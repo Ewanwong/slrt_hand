38b9b649c4571619c0d4ec562c5d6154085b3438
Ewanwong <903327884@qq.com>
2022-06-23 21:10:13 +0200

    mean

--- utils/ctc_loss.py
+++ utils/ctc_loss.py
@@ -6,7 +6,7 @@ def get_ctc_loss(alignments, valid_len, outputs, valid_output_len, blank_id=1295
     # alignments: batch, max_len, num_classes
     # outputs: batch, max_output_len, num_classes
 
-    ctc_loss = nn.CTCLoss(blank=blank_id, reduction='none', zero_infinity=True)
+    ctc_loss = nn.CTCLoss(blank=blank_id, reduction='mean', zero_infinity=True)
     log_probs = alignments.permute(1, 0, 2).log_softmax(2).requires_grad_()  # max_len, batch, num_classes 是否要detach
     _, target = torch.max(outputs, dim=2)
     loss = ctc_loss(log_probs, target, valid_len, valid_output_len)
